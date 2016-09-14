c74238d38f41754847f49cf6c38081c22109cc90
JeanaCee <chesj445@newschool.edu>
2016-09-13 21:44:36 -0400

    Message

---
+++ hw_week_2/js/script.js
@@ -0,0 +1,25 @@
+// Week 1 HW
+// function myMove() {
+//   var elem = document.getElementById("animate");
+//   var pos = 0;
+//   var id = setInterval(frame, 5);
+//   function frame() {
+//     if (pos == 350) {
+//       clearInterval(id);
+//     } else {
+//       pos++;
+//       elem.style.top = pos + 'px';
+//       elem.style.left = pos + 'px';
+//     }
+//   }
+// }
+
+
+// Week 2 HW ... failed code...
+// var folded = new OriDomi(document.getElementsByClassName('paper')[0]);
+
+// initialPosition: .5
+
+// showInstruction: true
+
+// instructiontext: "Click and Drag"
